d21c261299580d667057dc03788ee9476919ff79
Basil Njoga <Basilnjoga@gmail.com>
2022-06-26 20:31:18 +0300

    check

--- factors.c
+++ factors.c
@@ -35,17 +35,17 @@ for (int j = 0; j < k; j++)
     {
         n = numberline[j];
         while (i < n)
-        {
-            if (n % i == 0)
             {
-                n = n / i;
-                break;
+                if (n % i == 0)
+                {
+                    n = n / i;
+                    break;
+                }
+                else
+                {
+                i++;
+                }
             }
-            else
-            {
-            i++;
-            }
-        }
 printf("%llu=%llu*%llu\n", numberline[j], n, i);
 }
 return (0);
